66feaaa2cf03e6aa96eb2dcb844146677a9d1978
Abhijeet Anand <abhijeetanand628@gmail.com>
2025-09-11 01:27:24 +0530

    more functions

--- script.js
+++ script.js
@@ -2,34 +2,79 @@ const result = document.querySelector('.output')
 const input = document.querySelector('.history');
 const numbers = document.querySelector('.calculator-buttons')
 
+let isCalculated = false;
+
 
 result.innerHTML = '';    
 input.innerHTML = '';
 
 numbers.addEventListener('click', function(e) {
     const button = e.target;
+
     if(button.id === 'dltall')
     {
         input.innerHTML = '';
         result.innerHTML = '';
+        isCalculated = false;
+        return;
     }
 
     else if(button.id === 'dlt')
     {
         input.innerHTML = input.innerHTML.slice(0, -1);
+        return;
     }
 
-    else if(button.classList.contains('equals'))
+    if(button.classList.contains('equals'))
     {
-        result.innerHTML = eval(input.innerHTML)
+        if (input.innerHTML === '') 
+            return;
+
+        try {
+            result.innerHTML = eval(input.innerHTML);
+            isCalculated = true;
+        } catch(error)
+        {
+            result.innerHTML = "Error!";
+        }
+        return;
     }
 
-    else if(button.classList.contains('btn'))
+    if(button.classList.contains('operator'))
     {
-        input.innerHTML += button.innerHTML
+        if(isCalculated)
+        {
+            input.innerHTML = result.innerHTML + button.innerHTML;
+            isCalculated = false;
+        }
+        else
+        {
+            if(input.innerHTML === '' && button.innerHTML != '-')
+            {
+                alert("Enter a number first!");
+                return;
+            }
+            input.innerHTML += button.innerHTML
+        }
+        return;
     }
-    
+
+    if(button.classList.contains('btn'))
+    {
+        if(isCalculated)
+        {
+            input.innerHTML = button.innerHTML;
+            result.innerHTML = '';
+            isCalculated = false;
+        }
+        else
+        {
+            input.innerHTML += button.innerHTML;
+        }
+    }
 })
+
+
 
 
 
