2c9805469438af19b479b8c151f2130163a8b20d
Abhijeet Anand <abhijeetanand628@gmail.com>
2025-11-09 13:12:53 +0530

    Without using eval function

--- script.js
+++ script.js
@@ -1,78 +1,146 @@
 const result = document.querySelector('.output')
 const input = document.querySelector('.history');
-const numbers = document.querySelector('.calculator-buttons')
-
-let isCalculated = false;
+const buttons = document.querySelector('.calculator-buttons')
 
 
-result.innerHTML = '';    
-input.innerHTML = '';
+result.innerHTML = ''
+input.innerHTML = ''
 
-numbers.addEventListener('click', function(e) {
+const add = () => {
+    input.innerHTML
+}
+
+buttons.addEventListener('click', function(e) {
     const button = e.target;
-
-    if(button.id === 'dltall')
-    {
-        input.innerHTML = '';
-        result.innerHTML = '';
-        isCalculated = false;
+    
+    if(button.id === 'dltall'){
+        result.innerHTML  = ''
+        input.innerHTML = ''
         return;
     }
 
-    else if(button.id === 'dlt')
-    {
+    else if(button.id === 'dlt') {
         input.innerHTML = input.innerHTML.slice(0, -1);
         return;
     }
 
-    if(button.classList.contains('equals'))
-    {
-        if (input.innerHTML === '') 
+    if(button.classList.contains('equals')) {
+        const expression = input.innerHTML;
+
+        let operator;
+        if(expression.includes('+')) operator = '+'
+        else if (expression.includes('-')) operator = '-';
+        else if (expression.includes('*')) operator = '*';
+        else if (expression.includes('/')) operator = '/';
+        else return;
+
+        const [left, right] = expression.split(operator);
+        const num1 = parseFloat(left);
+        const num2 = parseFloat(right);
+
+        if (isNaN(num1) || isNaN(num2)) {
+            alert('Invalid input');
             return;
-
-        try {
-            result.innerHTML = eval(input.innerHTML);
-            isCalculated = true;
-        } catch(error)
-        {
-            result.innerHTML = "Error!";
         }
-        return;
-    }
 
-    if(button.classList.contains('operator'))
-    {
-        if(isCalculated)
-        {
-            input.innerHTML = result.innerHTML + button.innerHTML;
-            isCalculated = false;
-        }
-        else
-        {
-            if(input.innerHTML === '' && button.innerHTML != '-')
-            {
-                alert("Enter a number first!");
-                return;
+        let resultValue;
+            switch (operator) {
+            case '+':
+                resultValue = num1 + num2;
+                break;
+            case '-':
+                resultValue = num1 - num2;
+                break;
+            case '*':
+                resultValue = num1 * num2;
+                break;
+            case '/':
+                resultValue = num2 !== 0 ? num1 / num2 : 'Error';
+                break;
             }
-            input.innerHTML += button.innerHTML
-        }
-        return;
-    }
 
-    if(button.classList.contains('btn'))
-    {
-        if(isCalculated)
-        {
-            input.innerHTML = button.innerHTML;
-            result.innerHTML = '';
-            isCalculated = false;
+            result.innerHTML = resultValue;
+            input.innerHTML = resultValue; 
+            return;
         }
-        else
-        {
+    
+        if(button.classList.contains('btn')) {
             input.innerHTML += button.innerHTML;
         }
-    }
 })
+
+
+// let isCalculated = false;
+
+
+// result.innerHTML = '';    
+// input.innerHTML = '';
+
+// buttons.addEventListener('click', function(e) {
+//     const button = e.target;
+
+//     if(button.id === 'dltall')
+//     {
+//         input.innerHTML = '';
+//         result.innerHTML = '';
+//         isCalculated = false;
+//         return;
+//     }
+
+//     else if(button.id === 'dlt')
+//     {
+//         input.innerHTML = input.innerHTML.slice(0, -1);
+//         return;
+//     }
+
+//     if(button.classList.contains('equals'))
+//     {
+//         if (input.innerHTML === '') 
+//             return;
+
+//         try {
+//             result.innerHTML = eval(input.innerHTML);
+//             isCalculated = true;
+//         } catch(error)
+//         {
+//             result.innerHTML = "Error!";
+//         }
+//         return;
+//     }
+
+//     if(button.classList.contains('operator'))
+//     {
+//         if(isCalculated)
+//         {
+//             input.innerHTML = result.innerHTML + button.innerHTML;
+//             isCalculated = false;
+//         }
+//         else
+//         {
+//             if(input.innerHTML === '' && button.innerHTML != '-')
+//             {
+//                 alert("Enter a number first!");
+//                 return;
+//             }
+//             input.innerHTML += button.innerHTML
+//         }
+//         return;
+//     }
+
+//     if(button.classList.contains('btn'))
+//     {
+//         if(isCalculated)
+//         {
+//             input.innerHTML = button.innerHTML;
+//             result.innerHTML = '';
+//             isCalculated = false;
+//         }
+//         else
+//         {
+//             input.innerHTML += button.innerHTML;
+//         }
+//     }
+// })
 
 
 
